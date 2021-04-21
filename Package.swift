7d5ccf25ebfc507bd460fa393db80f5f91d072da
Gerson Noboa <gerson.noboa@glia.com>
2021-04-21 11:19:29 +0300

    Initial version

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "WebRTC",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTC"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC",
+            url: "https://github.com/salemove/ios-bundle/releases/download/0.27.0/WebRTC.xcframework.zip",
+            checksum: "996f02aff0f0ade1a16f0d8798150e58a126934ebdfd20610421931bfa459859"
+        ),
+        .target(
+            name: "WebRTC",
+            dependencies: [
+                "WebRTC"
+            ]
+        )
+    ]
+)
