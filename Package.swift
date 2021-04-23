784d94a45f50b5c96e6665f9526e673d6084b34b
Gerson Noboa <gerson.noboa@glia.com>
2021-04-23 13:06:44 +0300

    Static

--- Package.swift
+++ Package.swift
@@ -9,14 +9,13 @@ let package = Package(
     products: [
         .library(
             name: "WebRTC",
-            type: .dynamic,
             targets: ["WebRTCTarget"])
     ],
     targets: [
         .binaryTarget(
             name: "WebRTC",
             url: "https://github.com/salemove/ios-bundle/releases/download/0.27.0/WebRTC.xcframework.zip",
-            checksum: "996f02aff0f0ade1a16f0d8798150e58a126934ebdfd20610421931bfa459859"
+            checksum: "996f02aff0f0ade1a160d8798150e58a126934ebdfd20610421931bfa459859"
         ),
         .target(
             name: "WebRTCTarget",
